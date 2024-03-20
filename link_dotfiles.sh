c5d526fda29a8bae166eb7096d0604764f459b0d
{name} <{email}>
2024-03-20 21:49:45 +0900

    comment

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,6 +2,7 @@
 # bash
 echo LINK_BASH_CONFIG: ${LINK_BASH_CONFIG}
 if [[ ${LINK_BASH_CONFIG} == "true" ]] then
+    echo "Link bash configuration files."
     ln -nfs ~/dotfiles/bash/.bash_profile ~/.bash_profile
     ln -nfs ~/dotfiles/bash/.bashrc ~/.bashrc
 fi
@@ -9,6 +10,7 @@ fi
 # zsh
 echo LINK_ZSH_CONFIG: ${LINK_ZSH_CONFIG}
 if [[ ${LINK_ZSH_CONFIG} == "true" ]] then
+    echo "Link zsh configuration files."
     ln -nfs ~/dotfiles/zsh/.zprofile ~/.zprofile
     ln -nfs ~/dotfiles/zsh/.zshenv ~/.zshenv
     ln -nfs ~/dotfiles/zsh/.zshrc ~/.zshrc
@@ -18,6 +20,7 @@ fi
 # vim
 echo LINK_VIM_CONFIG: ${LINK_VIM_CONFIG}
 if [[ ${LINK_VIM_CONFIG} == "true" ]] then
+    echo "Link vim configuration files."
     ln -nfs ~/dotfiles/vim/.vimrc ~/.vimrc
 fi
 
@@ -25,6 +28,7 @@ fi
 # git
 echo LINK_GIT_CONFIG: ${LINK_GIT_CONFIG}
 if [[ ${LINK_GIT_CONFIG} == "true" ]] then
+    echo "Link git configuration files."
     ln -nfs ~/dotfiles/git/.gitconfig ~/.gitconfig
     ln -nfs ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 fi
