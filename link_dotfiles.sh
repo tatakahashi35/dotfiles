4fafce90ddd2773f1c43cb4d70261b17b593fb8b
tatakahashi35 <tata.lead.switch@gmail.com>
2024-03-20 21:42:42 +0900

    print settings

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,12 +1,14 @@
 # shell
 # bash
-if [[ $LINK_BASH_CONFIG == "true" ]] then
+echo LINK_BASH_CONFIG: ${LINK_BASH_CONFIG}
+if [[ ${LINK_BASH_CONFIG} == "true" ]] then
     ln -nfs ~/dotfiles/bash/.bash_profile ~/.bash_profile
     ln -nfs ~/dotfiles/bash/.bashrc ~/.bashrc
 fi
 
 # zsh
-if [[ $LINK_ZSH_CONFIG == "true" ]] then
+echo LINK_ZSH_CONFIG: ${LINK_ZSH_CONFIG}
+if [[ ${LINK_ZSH_CONFIG} == "true" ]] then
     ln -nfs ~/dotfiles/zsh/.zprofile ~/.zprofile
     ln -nfs ~/dotfiles/zsh/.zshenv ~/.zshenv
     ln -nfs ~/dotfiles/zsh/.zshrc ~/.zshrc
@@ -14,13 +16,15 @@ fi
 
 # editor
 # vim
-if [[ $LINK_VIM_CONFIG == "true" ]] then
+echo LINK_VIM_CONFIG: ${LINK_VIM_CONFIG}
+if [[ ${LINK_VIM_CONFIG} == "true" ]] then
     ln -nfs ~/dotfiles/vim/.vimrc ~/.vimrc
 fi
 
 # other application
 # git
-if [[ $LINK_GIT_CONFIG == "true" ]] then
+echo LINK_GIT_CONFIG: ${LINK_GIT_CONFIG}
+if [[ ${LINK_GIT_CONFIG} == "true" ]] then
     ln -nfs ~/dotfiles/git/.gitconfig ~/.gitconfig
     ln -nfs ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 fi
