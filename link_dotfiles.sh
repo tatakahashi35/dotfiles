0be3a2c9d3184c83e8ccd50d96105aa55178a808
{name} <{email}>
2024-03-20 21:50:59 +0900

    comment

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -6,6 +6,7 @@ if [[ ${LINK_BASH_CONFIG} == "true" ]] then
     ln -nfs ~/dotfiles/bash/.bash_profile ~/.bash_profile
     ln -nfs ~/dotfiles/bash/.bashrc ~/.bashrc
 fi
+echo
 
 # zsh
 echo LINK_ZSH_CONFIG: ${LINK_ZSH_CONFIG}
@@ -15,6 +16,7 @@ if [[ ${LINK_ZSH_CONFIG} == "true" ]] then
     ln -nfs ~/dotfiles/zsh/.zshenv ~/.zshenv
     ln -nfs ~/dotfiles/zsh/.zshrc ~/.zshrc
 fi
+echo
 
 # editor
 # vim
@@ -23,6 +25,7 @@ if [[ ${LINK_VIM_CONFIG} == "true" ]] then
     echo "Link vim configuration files."
     ln -nfs ~/dotfiles/vim/.vimrc ~/.vimrc
 fi
+echo
 
 # other application
 # git
@@ -32,3 +35,4 @@ if [[ ${LINK_GIT_CONFIG} == "true" ]] then
     ln -nfs ~/dotfiles/git/.gitconfig ~/.gitconfig
     ln -nfs ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 fi
+echo
