17e24552e42535aaf0fcdb6746e4bb2b2b92b818
tatakahashi35 <tata.lead.switch@gmail.com>
2025-05-06 08:37:33 +0900

    Fix paths for linking configuration files in link_dotfiles.sh

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -3,8 +3,8 @@
 echo LINK_BASH_CONFIG: ${LINK_BASH_CONFIG}
 if [[ ${LINK_BASH_CONFIG} == "true" ]] then
     echo "Link bash configuration files."
-    ln -nfs ~/dotfiles/bash/.bash_profile ~/.bash_profile
-    ln -nfs ~/dotfiles/bash/.bashrc ~/.bashrc
+    ln -nfs ~/dotfiles/apps/bash/.bash_profile ~/.bash_profile
+    ln -nfs ~/dotfiles/apps/bash/.bashrc ~/.bashrc
 fi
 echo
 
@@ -12,9 +12,9 @@ echo
 echo LINK_ZSH_CONFIG: ${LINK_ZSH_CONFIG}
 if [[ ${LINK_ZSH_CONFIG} == "true" ]] then
     echo "Link zsh configuration files."
-    ln -nfs ~/dotfiles/zsh/.zprofile ~/.zprofile
-    ln -nfs ~/dotfiles/zsh/.zshenv ~/.zshenv
-    ln -nfs ~/dotfiles/zsh/.zshrc ~/.zshrc
+    ln -nfs ~/dotfiles/apps/zsh/.zprofile ~/.zprofile
+    ln -nfs ~/dotfiles/apps/zsh/.zshenv ~/.zshenv
+    ln -nfs ~/dotfiles/apps/zsh/.zshrc ~/.zshrc
 fi
 echo
 
@@ -23,7 +23,7 @@ echo
 echo LINK_VIM_CONFIG: ${LINK_VIM_CONFIG}
 if [[ ${LINK_VIM_CONFIG} == "true" ]] then
     echo "Link vim configuration files."
-    ln -nfs ~/dotfiles/vim/.vimrc ~/.vimrc
+    ln -nfs ~/dotfiles/apps/vim/.vimrc ~/.vimrc
 fi
 echo
 
@@ -32,7 +32,7 @@ echo
 echo LINK_GIT_CONFIG: ${LINK_GIT_CONFIG}
 if [[ ${LINK_GIT_CONFIG} == "true" ]] then
     echo "Link git configuration files."
-    ln -nfs ~/dotfiles/git/.gitconfig ~/.gitconfig
-    ln -nfs ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+    ln -nfs ~/dotfiles/apps/git/.gitconfig ~/.gitconfig
+    ln -nfs ~/dotfiles/apps/git/.gitignore_global ~/.gitignore_global
 fi
 echo
