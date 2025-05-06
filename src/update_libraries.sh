5c65f6a76ae85205200c6854938728517e4e49a9
tatakahashi35 <tata.lead.switch@gmail.com>
2025-05-06 11:41:03 +0900

    Update library versions

--- src/update_libraries.sh
+++ src/update_libraries.sh
@@ -9,12 +9,14 @@ git switch -c update_library
 
 # Update Library versions
 ## brew
+echo "Updating brew ..."
 cd apps/brew
 zsh update_Brewfile.sh
 cd -
 git add apps/brew/Brewfile
 
 ## vscode
+echo "Updating vscode ..."
 cd apps/vscode
 zsh update_vscode_extensions.sh
 cd -
@@ -25,6 +27,7 @@ git commit -m "Update library versions"
 git push -f origin update_library
 
 # Create a pull request
+echo "Creating PR"
 gh pr create --base main --head update_library --title "Update library" --body "Update library"
 
 # Clean up
